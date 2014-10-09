112b7695cb9d3864db19fa4ffe38bac38a095570
gfixler <gary@garyfixler.com>
2014-10-09 08:54:52 -0700

    WIP on Exercise 7 solution for hanoi with 4 pegs

--- hw01/HW01.hs
+++ hw01/HW01.hs
@@ -47,3 +47,12 @@ hanoi n a b c
     | n == 0    = []
     | otherwise = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
 
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 n a b c d
+    | n < 1     = []
+    | otherwise = hanoi4 (n-2) a c d b ++
+                  hanoi4 (n-1) c b a d ++
+                  [(a,b)] ++
+                  hanoi4 (n-1) c b a d ++
+                  hanoi4 (n-2) c b a d
+
