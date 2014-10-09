5444aecfe61be27c64e4ae63e772d7d70c515ba6
gfixler <gary@garyfixler.com>
2014-10-09 00:55:10 -0700

    Add Exercise 6 solution for hanoi

--- hw01/HW01.hs
+++ hw01/HW01.hs
@@ -38,3 +38,12 @@ validate :: Integer -> Bool
 validate n = checksum `mod` 10 == 0
     where checksum = sumDigits . doubleEveryOther . toDigits $ n
 
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n a b c
+    | n == 0    = []
+    | otherwise = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
+
