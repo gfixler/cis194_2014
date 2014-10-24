9b80da910541841000464d3f14d4d5f89ec7c3b7
gfixler <gary@garyfixler.com>
2014-10-24 04:18:13 -0700

    Add Exercise 1 solution with epic Vim undo file
    This took so much iteration to get working, I decided to make and save
    a Vim undo file along with it for posterity. Feel my pain, future-folk.

--- hw03/LogAnalysis.hs
+++ hw03/LogAnalysis.hs
@@ -1,7 +1,28 @@
 {-# OPTIONS_GHC -Wall #-}
+
 module LogAnalysis where
 
 import Log
 
+handleError :: String -> MaybeInt -> MaybeInt -> [String] -> MaybeLogMessage
+handleError _ (ValidInt l) (ValidInt t) xs =
+    ValidLM (LogMessage (Error l) t (unwords xs))
+handleError s _ _ _ = InvalidLM s
+
+handleInfo :: String -> MaybeInt -> [String] -> MaybeLogMessage
+handleInfo _ (ValidInt t) xs = ValidLM (LogMessage Info t (unwords xs))
+handleInfo s _ _             = InvalidLM s
+
+handleWarning :: String -> MaybeInt -> [String] -> MaybeLogMessage
+handleWarning _ (ValidInt t) xs = ValidLM (LogMessage Warning t (unwords xs))
+handleWarning s _ _             = InvalidLM s
+
+parseMessage' :: [String] -> MaybeLogMessage
+parseMessage' s@("E":l:t:xs) = handleError (unwords s) (readInt l) (readInt t) xs
+parseMessage' s@("I":t:xs)   = handleInfo (unwords s) (readInt t) xs
+parseMessage' s@("W":t:xs)   = handleWarning (unwords s) (readInt t) xs
+parseMessage' s              = InvalidLM (unwords s)
+
 parseMessage :: String -> MaybeLogMessage
+parseMessage = parseMessage' . words
 
