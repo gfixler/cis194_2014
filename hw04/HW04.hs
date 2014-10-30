a45cd36bed0ad9abbb19f6650ef31ee40d990733
gfixler <gary@garyfixler.com>
2014-10-30 01:40:54 -0700

    Add solution for Exercise 1

---
+++ hw04/HW04.hs
@@ -0,0 +1,10 @@
+-- Name: Gary Fixler
+
+module HW04 where
+
+-- Exercise 1
+-- There is no source for bs; we must return the one we get.
+-- This is basically the identity function with an extra, useless argument.
+ex1 :: a -> b -> b
+ex1 _ x = x
+
